8f05a22bf8c4bfd6abdf727168639f290a8d9f00
Pierce Faraone <pf4tj@virginia.edu>
2019-02-12 19:39:51 -0500

    mystery_socket creation and implementation

--- testing.py
+++ testing.py
@@ -10,33 +10,34 @@ while True:
         server_address = ('', 21212)
         sock.bind(server_address)
         sock.listen(1)
-        print('the server is ready to receive ')
-        connection, addr =  sock.accept()
-        print('connected by : ', addr)
-        while True: 
-            message = conn.recv(1024)
-            if not message:
-                print('there is no data')
-                break
-            # connection.sendall(data)
+    except: 
+        print('failed to create socket ...')
+        sock.close()
+        break
+    print('the server is ready to receive ')
+    connection, addr =  sock.accept()
+    print('connected by : ', addr)
+    while True: 
+        message = connection.recv(1024)
+        if not message:
+            print('there is no data')
+            break
+        message = message.decode()
+        start = message.find('PASSWORD=')
+        if (start != -1):
+            message = message[start + 9:]
+            end  = message.find('.')
+            print(message[:end])
+            break
+    print('Socket closing ... ')
+    sock.close()
+
+
+
+                    # connection.sendall(data)
             # password = 'Sup3rSecre$'
             # r1 = ''.join(random.choices(string.printable, k=30))
             # r2 = ''.join(random.choices(string.printable, k=20))
             # message = '{}PASSWORD={}.{}'.format(r1, password, r2)
             # print ('encrypted message is : ', message)
             # print('Encrypted message is : ',message, '\n')
-            start = message.find('PASSWORD=')
-            if (start != -1):
-                message = message[start + 9:]
-                end  = message.find('.')
-                print(message[:end])
-                break
-
-    except: 
-        print('exception occured')
-        sock.close()
-        exit()
-
-    finally:
-        print('Socket closing ... ')
-        sock.close()
